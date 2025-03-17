942c90d3cb12d2d5b633c1430c5b43deb481d870
TitasBSP <titasbsp@gmail.com>
2025-03-17 17:12:27 +0100

    Binary done, now do decimal

--- binary-converter.c
+++ binary-converter.c
@@ -15,40 +15,83 @@ int main() {
     int binaryArray[8] = {0}; // The array for the actual binary numbers is stored in this array
     int inputNumber = 0; // The number which the user will input in the "scanf()" below
     int binaryNumbers = 128; // Used to divide the input number in while for loop ahead!
+    int decimalNumbers = 128;
+    char character;
+    char inputString[9];
+    int outputDecimal = 0;
 
     printf("Welcome to the decimal to binary converter!\n"); // Welcome text!
 
     while (programRunning == true) { // PRIMARY LOOP FOR THE PROGRAM
-        clearTerminal();
-        printf("Please enter a number to convert (1-255): "); // Asks for input
-        scanf("%d", &inputNumber); // Keyword for input
-        int number = inputNumber;
+        printf("Please choose your designated conversion:\n");
+        printf("[D] - Binary ---> Decimal\n");
+        printf("[B] - Decimal ---> Binary\n:: ");
+        character = getchar();
 
-        if (number <= 255) { // If-statement where the magic happens
-            for (int binaryCounter = 0; binaryCounter < 8; ++binaryCounter) { // binaryCounter goes through all 8 digits of the binary table!
-                binaryArray[binaryCounter] = (number / binaryNumbers) ? 1 : 0; // The selected binary number is now checked if it is divisible by the current binaryNumber, right now 128, and if so, it is given a 1
-                    if (binaryArray[binaryCounter]) {
-                        number -= binaryNumbers; // The current number is subtracted with the binary sequence number and the loop is repeated until binaryCounter goes up to 8
-                    }
-                    binaryNumbers /= 2; // If the inputNumber is indivisible by the current one, it will be divided by two
-            }
-            clearTerminal();
-            printf("The binary equivalent to [%d]", inputNumber);
-            printf(" is: "); // Here's the prompt which the user will see
-
-            for (int i = 0; i < 8; i++) { // Since whole arrays cannot be printed, they have to be fragmented into a for-loop which prints out the elements one by one
-                printf("%d", binaryArray[i]);
-            }
-            printf("\n");
-            Sleep(5000);
-            clearTerminal();
-
-        } else { // If the number which was entered is higher than 255, the program will notify the user and loop back to the beginning!
-            clearTerminal();
-            printf("We're sorry, but the number that you entered exceeds the binary limit!");
+        if (character == 'D' || 'd') {
+            printf("%c", character);
             Sleep(2000);
-        }
+            clearTerminal();
+            printf("Please enter a BINARY number to convert (00000000 - 11111111): "); // Asks for input
+            scanf("%s", &inputString); // Keyword for input
 
+            // Make program check if input has 8 characters
+            if (strlen(inputString) == 8) {
+
+                for (int binaryCounter = 0; binaryCounter < 8; ++binaryCounter) {
+                    if (inputString[binaryCounter] == '0') {
+                        decimalNumbers /= 2; // 11110000
+                        continue;
+                    } else if (inputString[binaryCounter == '1']) {
+                        decimalNumbers += outputDecimal;
+                        decimalNumbers /= 2;
+                    }
+                };
+                clearTerminal();
+                printf("The decimal equivalent to [%s]\n", inputString);
+                printf("is: %d", outputDecimal); // Here's the prompt which the user will see
+                Sleep(5000);
+
+            } else {
+                printf("INVALID BINARY NUMBER, try again!");
+                Sleep(1000);
+            }
+
+            // Make for loop to loop through the integers and see if they are 0 or 1
+
+            // Make for loop to loop through and add the numbers together!
+
+        } else if (character == 'B' || 'b') {
+            clearTerminal();
+            printf("Please enter a DECIMAL number to convert (1 - 255): "); // Asks for input
+            scanf("%d", &inputNumber); // Keyword for input
+            int number = inputNumber;
+
+            if (number <= 255) { // If-statement where the magic happens
+                for (int binaryCounter = 0; binaryCounter < 8; ++binaryCounter) { // binaryCounter goes through all 8 digits of the binary table!
+                    binaryArray[binaryCounter] = (number / binaryNumbers) ? 1 : 0; // The selected binary number is now checked if it is divisible by the current binaryNumber, right now 128, and if so, it is given a 1
+                        if (binaryArray[binaryCounter]) {
+                            number -= binaryNumbers; // The current number is subtracted with the binary sequence number and the loop is repeated until binaryCounter goes up to 8
+                        }
+                        binaryNumbers /= 2; // If the inputNumber is indivisible by the current one, it will be divided by two
+                }
+                clearTerminal();
+                printf("The binary equivalent to [%d]", inputNumber);
+                printf(" is: "); // Here's the prompt which the user will see
+
+                for (int i = 0; i < 8; i++) { // Since whole arrays cannot be printed, they have to be fragmented into a for-loop which prints out the elements one by one
+                    printf("%d", binaryArray[i]);
+                }
+                printf("\n");
+                Sleep(8000);
+                clearTerminal();
+
+            } else { // If the number which was entered is higher than 255, the program will notify the user and loop back to the beginning!
+                clearTerminal();
+                printf("We're sorry, but the number that you entered exceeds the binary limit!");
+                Sleep(2000);
+            }
+        }
     };
     return 0;
 }
