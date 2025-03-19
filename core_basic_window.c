f125be821873feaa3102b60402c47229d54509ac
TitasBSP <titasbsp@gmail.com>
2025-03-19 13:51:10 +0100

    GUI with buttons

--- core_basic_window.c
+++ core_basic_window.c
@@ -16,11 +16,15 @@
 ********************************************************************************************/
 
 #include "raylib.h"
+#include "stdio.h"
 
 int main()
 {
     // Initialization
     //--------------------------------------------------------------------------------------
+    
+    bool DrawScreen = true;
+
     int screenWidth = 960;
     int screenHeight = 540;
     
@@ -30,7 +34,8 @@ int main()
     
     float ButtonBinaryX = 300;
     float ButtonBinaryY = 220;
-    
+    Rectangle ButtonBinary = {ButtonBinaryX, ButtonBinaryY, 300, 75};
+
     float alphaButtonBinary = 0.0f;
     
     float TextBinaryX = 350;
@@ -39,8 +44,15 @@ int main()
     
     float ButtonDecimalX = 300;
     float ButtonDecimalY = 350;
+    Rectangle ButtonDecimal = {ButtonDecimalX, ButtonDecimalY, 300, 75};
     
     float alphaButtonDecimal = 0.0f;
+    
+    float TextDecimalX = 340;
+    float TextDecimalY = 365;
+    char TextDecimal[8] = "DECIMAL";
+    
+    char Copyright[64] = "© 2025 - Elevens";
     
    
     char title[64] = "Binary & Decimal Converter";
@@ -65,13 +77,37 @@ int main()
         } else {
             if (alphaButtonBinary < 1.0f) alphaButtonBinary += 0.02f;
             DrawRectangle(310, 230, 300, 75, Fade(BLACK, alphaButtonBinary));
-            DrawRectangle(ButtonBinaryX, ButtonBinaryY, 300, 75, Fade(WHITE, alphaButtonBinary));
+            DrawRectangleRec(ButtonBinary, Fade(WHITE, alphaButtonBinary));
             DrawText(TextBinary, TextBinaryX, TextBinaryY, 50, Fade(BLACK, alphaButtonBinary));
             
             if (alphaButtonDecimal < 1.0f) alphaButtonDecimal += 0.02f;
             DrawRectangle(310, 360, 300, 75, Fade(BLACK, alphaButtonDecimal));
-            DrawRectangle(ButtonDecimalX, ButtonDecimalY, 300, 75, Fade(WHITE, alphaButtonDecimal));
+            DrawRectangleRec(ButtonDecimal, Fade(WHITE, alphaButtonDecimal));
+            DrawText(TextDecimal, TextDecimalX, TextDecimalY, 50, Fade(BLACK, alphaButtonDecimal));
+            DrawText(Copyright, 360, 500, 20, Fade(WHITE, alphaButtonDecimal));
         }
+        
+        Vector2 mousePos = GetMousePosition();
+        
+        bool isHoveringBinary = CheckCollisionPointRec(mousePos, ButtonBinary);
+        bool isHoveringDecimal = CheckCollisionPointRec(mousePos, ButtonDecimal);
+        
+        if (isHoveringBinary) {
+            DrawRectangleRec(ButtonBinary, Fade(LIGHTGRAY, alphaButtonBinary));
+            DrawText(TextBinary, TextBinaryX, TextBinaryY, 50, Fade(BLACK, alphaButtonBinary));
+        } else {
+            DrawRectangleRec(ButtonBinary, Fade(WHITE, alphaButtonDecimal));
+            DrawText(TextBinary, TextBinaryX, TextBinaryY, 50, Fade(BLACK, alphaButtonBinary));
+        }
+        
+        if (isHoveringDecimal) {
+            DrawRectangleRec(ButtonDecimal, Fade(LIGHTGRAY, alphaButtonBinary));
+            DrawText(TextDecimal, TextDecimalX, TextDecimalY, 50, Fade(BLACK, alphaButtonBinary));
+        } else {
+            DrawRectangleRec(ButtonDecimal, Fade(WHITE, alphaButtonDecimal));
+            DrawText(TextDecimal, TextDecimalX, TextDecimalY, 50, Fade(BLACK, alphaButtonBinary));
+        }
+        
         
         
         BeginDrawing();
@@ -79,6 +115,11 @@ int main()
             ClearBackground(BLUE);
             
             DrawText(title, titleX, titleY, titleSize, WHITE);
+            
+            if (isHoveringBinary && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                ClearBackground(BLUE);
+                printf("Button Clicked!\n");
+            }
 
         EndDrawing();
         //----------------------------------------------------------------------------------
